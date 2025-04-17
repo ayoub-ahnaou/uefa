f84337d0016edc2ea3c00ea85fd76f687b1c890c
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-04-17 16:26:14 +0100

    feat: add Hero component with initial content and styling

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-blue-600">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, qui
+      dolores quos aspernatur neque dolorum in error et voluptas ab dolor
+      exercitationem ipsum alias minus quis dolorem eos quia id?
+    </div>
+  );
+};
+
+export default Hero;
