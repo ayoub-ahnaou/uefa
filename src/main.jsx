b0315cf2d946b211cdf8bddd0047d97ed4c470de
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-04-17 16:26:04 +0100

    refactor: implement React Router for navigation and restructure main rendering logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import ScoresPage from "./components/ScoresPage.jsx";
+import MatchDetailsPage from "./components/MatchDetailsPage.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<ScoresPage />} />
+      <Route path="/match/:id" element={<MatchDetailsPage />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
